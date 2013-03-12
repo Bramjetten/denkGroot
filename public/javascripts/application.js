3f4afbe5332149cddfb4712cac478d02ba800d78
Bram Jetten <mail@bramjetten.nl>
2013-03-12 13:24:16 +0100

    In het midden!

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,27 @@
 jQuery(document).ready(function($) {
 
+  function sniffer() {
+    var introduction = $('.introduction');
+    var windowHeight = $(window).height();
+    var introHeight = introduction.height();
+
+    introduction.css("top", windowHeight / 2 - introHeight / 2 + "px");
+
+  }
+  
+  sniffer();
+
   $("nav a, .get-to-know-us").bind('click', function(e) {
     $('html,body').stop().animate({
       scrollTop: $($(this).attr('href')).offset().top
     }, 800, "easeInOutExpo");
 
     e.preventDefault();
+  });
+
+  //on window resize event
+  $(window).resize(function(){
+    sniffer();
   });
 
 });

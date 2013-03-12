72b688a34733afed81069a4d36d48cd3ff3aeaa8
Bram Jetten <mail@bramjetten.nl>
2013-03-12 13:09:07 +0100

    Cleanup

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,14 +1,8 @@
 jQuery(document).ready(function($) {
 
   $("nav a, .get-to-know-us").bind('click', function(e) {
-    var $anchor = $(this);
-
-    $('[data-spy="scroll"]').each(function() {
-      var $spy = $(this).scrollspy('refresh')
-    });
-
     $('html,body').stop().animate({
-      scrollTop: $($anchor.attr('href')).offset().top
+      scrollTop: $($(this).attr('href')).offset().top
     }, 800, "easeInOutExpo");
 
     e.preventDefault();

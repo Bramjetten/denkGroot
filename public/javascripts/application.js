1e731d3fc2188221f7c80cbfc568f68e8956a5a3
Bram Jetten <mail@bramjetten.nl>
2013-03-12 13:07:41 +0100

    Scrollspy

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,6 +1,17 @@
 jQuery(document).ready(function($) {
-  $("nav a, .get-to-know-us").click(function(e){
+
+  $("nav a, .get-to-know-us").bind('click', function(e) {
+    var $anchor = $(this);
+
+    $('[data-spy="scroll"]').each(function() {
+      var $spy = $(this).scrollspy('refresh')
+    });
+
+    $('html,body').stop().animate({
+      scrollTop: $($anchor.attr('href')).offset().top
+    }, 800, "easeInOutExpo");
+
     e.preventDefault();
-    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 800, "easeInOutExpo");
   });
+
 });
